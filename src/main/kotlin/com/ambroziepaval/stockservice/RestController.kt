e4063f8375a90978403a82d6f5a13fb74b802fe4
Paval, Ambrozie <ambroziepaval@gmail.com>
2020-03-01 15:46:09 +0200

    Add RSocket Controller.

--- src/main/kotlin/com/ambroziepaval/stockservice/RestController.kt
+++ src/main/kotlin/com/ambroziepaval/stockservice/RestController.kt
@@ -1,26 +1,23 @@
 package com.ambroziepaval.stockservice
 
 import org.springframework.http.MediaType
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Flux
-import java.time.Duration
-import java.time.LocalDateTime
-import java.util.concurrent.ThreadLocalRandom
 
 @RestController
-class RestController {
+class RestController(val priceService: PriceService) {
 
     @GetMapping(value = ["/stocks/{symbol}"],
             produces = [MediaType.TEXT_EVENT_STREAM_VALUE])
-    fun pricesFor(@PathVariable symbol: String): Flux<StockPrice> {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map { StockPrice(symbol, generateRandomPrice(), LocalDateTime.now()) }
-                .share()
-    }
+    fun pricesFor(@PathVariable symbol: String) = priceService.getPrices(symbol)
+}
 
-    private fun generateRandomPrice(): Double {
-        return ThreadLocalRandom.current().nextDouble(100.0)
-    }
+@Controller
+class RSocketController(val priceService: PriceService) {
+
+    @MessageMapping("stockPrices")
+    fun pricesFor(symbol: String) = priceService.getPrices(symbol)
 }
