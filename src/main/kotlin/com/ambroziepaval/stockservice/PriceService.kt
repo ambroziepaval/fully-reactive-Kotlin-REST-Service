64d0a3da210a84849ea40e2780e0817f7087341d
Paval, Ambrozie <ambroziepaval@gmail.com>
2020-03-01 16:16:12 +0200

    Log new subscriptions & introduce map for prices to reuse existing Flux.

--- src/main/kotlin/com/ambroziepaval/stockservice/PriceService.kt
+++ src/main/kotlin/com/ambroziepaval/stockservice/PriceService.kt
@@ -1,18 +1,27 @@
 package com.ambroziepaval.stockservice
 
+import org.apache.commons.logging.LogFactory
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import java.time.Duration
 import java.time.LocalDateTime
+import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.ThreadLocalRandom
 
 @Service
 class PriceService {
 
+    private val pricesForStock = ConcurrentHashMap<String, Flux<StockPrice>>()
+    private val log = LogFactory.getLog(javaClass)
+
     fun getPrices(symbol: String): Flux<StockPrice> {
-        return Flux.interval(Duration.ofSeconds(1))
-                .map { StockPrice(symbol, generateRandomPrice(), LocalDateTime.now()) }
-                .share()
+
+        return pricesForStock.computeIfAbsent(symbol) {
+            Flux.interval(Duration.ofSeconds(1))
+                    .map { StockPrice(symbol, generateRandomPrice(), LocalDateTime.now()) }
+                    .doOnSubscribe { log.info("New subscription for symbol $symbol.") }
+                    .share()
+        }
     }
 
     private fun generateRandomPrice(): Double {
